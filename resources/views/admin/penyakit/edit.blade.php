003d6c18dbbe97d5583d05aaa21a2d5492482d6e
muhammadasadbastomi <tomycules@gmail.com>
2021-06-29 10:53:45 +0800

    penyakit crud

--- resources/views/admin/penyakit/edit.blade.php
+++ resources/views/admin/penyakit/edit.blade.php
@@ -1,31 +1,36 @@
 @extends('layouts.main')
 @section('content')
 <div class="main-content container-fluid">
-                <div class="page-title">
-                    <h3>Jenis diklat / Data</h3>
-                </div>
-                <section class="section mt-4">
-                  <div class="card">
-                    <div class="card-header text-right">
-                        Form EDit
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="">Nama Penyakit</label>
-                            <input type="text" class="form-control">
-                          </div>
-                          <div class="form-group">
-                            <label for="">Uraian</label>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <a href="{{Route('userAdmin.jenis_diklat.index')}}" class="btn icon icon-left btn-secondary"><i data-feather="arrow-left"></i> Kembali</a>
-                        <button class="btn icon icon-left btn-primary"><i data-feather="save"></i> Ubah Data</button>
-                    </div>
-                  </div>
-                </section>
-            </div>
+  <div class="page-title">
+    <h3>Jenis diklat / Data</h3>
+  </div>
+  <section class="section mt-4">
+    <div class="card">
+      <div class="card-header text-right">
+        Form EDit
+      </div>
+      <form action="{{route('userAdmin.penyakit.update',$penyakit->id)}}" method="POST">
+        @csrf
+        @method('put')
+        <div class="card-body">
+          <div class="form-group">
+            <label for="">Nama Penyakit</label>
+            <input type="text" name="nama_penyakit" value="{{$penyakit->nama_penyakit}}" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="">Uraian</label>
+            <input type="text" name="uraian" value="{{$penyakit->uraian}}" class="form-control">
+          </div>
+        </div>
+        <div class="card-footer">
+          <a href="{{Route('userAdmin.jenis_diklat.index')}}" class="btn icon icon-left btn-secondary"><i
+              data-feather="arrow-left"></i> Kembali</a>
+          <button class="btn icon icon-left btn-primary"><i data-feather="save"></i> Ubah Data</button>
+        </div>
+      </form>
+    </div>
+  </section>
+</div>
 
 @endsection
 @section('script')
--- resources/views/admin/penyakit/index.blade.php
+++ resources/views/admin/penyakit/index.blade.php
@@ -1,83 +1,88 @@
 @extends('layouts.main')
 @section('content')
 <div class="main-content container-fluid">
-                <div class="page-title">
-                    <h3>Penyakit Bawaan / Data</h3>
-                </div>
-                <section class="section mt-4">
-                  <div class="card">
-                    <div class="card-header text-right">
-                        <button type="button" class="btn btn-outline-primary block" data-bs-toggle="modal"
-                            data-bs-target="#default">
-                            Tambah Data
-                        </button>
+    <div class="page-title">
+        <h3>Penyakit Bawaan / Data</h3>
+    </div>
+    <section class="section mt-4">
+        <div class="card">
+            <div class="card-header text-right">
+                <button type="button" class="btn btn-outline-primary block" data-bs-toggle="modal"
+                    data-bs-target="#default">
+                    Tambah Data
+                </button>
 
-                    </div>
-                    <div class="card-body">
-                    <table class='table table-striped' id="table1">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama penyakit</th>
-                                        <th>Uraian</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Jantung</td>
-                                        <td>-</td>
-                                        <td>
-                                            <a href="{{Route('userAdmin.penyakit.edit','1')}}" class="btn icon icon-left btn-primary"><i data-feather="edit"></i>
-                                            Edit</a>
-                                            <a href="#" class="btn icon icon-left btn-danger"><i data-feather="delete"></i>
-                                            Hapus</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                    </div>
-                  </div>
-                </section>
             </div>
+            <div class="card-body">
+                <table class='table table-striped' id="table1">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama penyakit</th>
+                            <th>Uraian</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $d)
 
-            <!--Basic Modal -->
-            <div class="modal fade text-left" id="default" tabindex="-1" role="dialog"
-                aria-labelledby="myModalLabel1" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-scrollable" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="myModalLabel1">Tambah Data</h5>
-                            <button type="button" class="close rounded-pill"
-                                data-bs-dismiss="modal" aria-label="Close">
-                                <i data-feather="x"></i>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                          <div class="form-group">
-                            <label for="">Nama Penyakit</label>
-                            <input type="text" class="form-control">
-                          </div>
-                          <div class="form-group">
-                            <label for="">Uraian</label>
-                            <input type="text" class="form-control">
-                          </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">
-                                <i class="bx bx-x d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Batal</span>
-                            </button>
-                            <button type="button" class="btn btn-primary ml-1"
-                                data-bs-dismiss="modal">
-                                <i class="bx bx-check d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Simpan Data</span>
-                            </button>
-                        </div>
+                        <tr>
+                            <td>{{$loop->iteration}}</td>
+                            <td>{{$d->nama_penyakit}}</td>
+                            <td>{{$d->uraian}}</td>
+                            <td>
+                                <a href="{{Route('userAdmin.penyakit.edit',$d->id)}}"
+                                    class="btn icon icon-left btn-primary"><i data-feather="edit"></i>
+                                    Edit</a>
+                                <a href="#" class="btn icon icon-left btn-danger"><i data-feather="delete"></i>
+                                    Hapus</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!--Basic Modal -->
+<div class="modal fade text-left" id="default" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <form action="{{route('userAdmin.penyakit.store')}}" method="POST">
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myModalLabel1">Tambah Data</h5>
+                    <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                        <i data-feather="x"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Penyakit</label>
+                        <input type="text" name="nama_penyakit" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Uraian</label>
+                        <input type="text" name="uraian" class="form-control">
                     </div>
                 </div>
-            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn" data-bs-dismiss="modal">
+                        <i class="bx bx-x d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Batal</span>
+                    </button>
+                    <button type="submit" class="btn btn-primary ml-1">
+                        <i class="bx bx-check d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Simpan Data</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
 @section('script')
 <script>
